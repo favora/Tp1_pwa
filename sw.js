9ae319dfb066b3d3cd38b74a98d446f760fcd861
brucie lekedji <brucylekedji@gmail.com>
2023-01-05 14:24:05 +0100

    fix: config for git

--- sw.js
+++ sw.js
@@ -1,12 +1,19 @@
 const CACHE_NAME = `tp1-app-cache-v1`;
-const urlsToCache = ["index.html", "manifest.json", "sw.js", "style.css", "icon.png"];
-
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "sw.js",
+  "style.css",
+  "icon.png",
+];
+console.log("sw");
 // Use the install event to pre-cache all initial resources.
 self.addEventListener("install", (event) => {
   event.waitUntil(
     (async () => {
       const cache = await caches.open(CACHE_NAME);
       cache.addAll(urlsToCache);
+      console.log("install");
     })()
   );
 });
@@ -22,15 +29,14 @@ self.addEventListener("fetch", (event) => {
 
         // Save the resource in the cache.
         cache.put(event.request, fetchResponse.clone());
-        if(navigator.offLine)
-        console.log('en ligne');
+        console.log("en ligne");
 
         // And return it.
         return fetchResponse;
       } catch (e) {
         // Fetching didn't work get the resource from the cache.
         const cachedResponse = await cache.match(event.request);
-        console.log('Hors ligne');
+        console.log("Hors ligne");
 
         // And return it.
         return cachedResponse;
@@ -39,8 +45,10 @@ self.addEventListener("fetch", (event) => {
   );
 });
 
+self.addEventListener("offline", (e) => {
+  console.log("offline");
+});
 
-self.addEventListener('offline', (e) => { console.log('offline'); });
-
-self.addEventListener('online', (e) => { console.log('online'); });
-
+self.addEventListener("online", (e) => {
+  console.log("online");
+});
