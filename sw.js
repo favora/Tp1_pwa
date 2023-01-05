c5fc81f15efe53d59b941ccb3ce856b30a1819a8
brucie lekedji <brucylekedji@gmail.com>
2023-01-05 12:28:43 +0100

    feat: manifest & sw

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = `tp1-app-cache-v1`;
+const urlsToCache = ["index.html", "manifest.json", "sw.js", "style.css", "icon.png"];
+
+// Use the install event to pre-cache all initial resources.
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(urlsToCache);
+    })()
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      try {
+        // Try to fetch the resource from the network.
+        const fetchResponse = await fetch(event.request);
+
+        // Save the resource in the cache.
+        cache.put(event.request, fetchResponse.clone());
+        console.log('en ligne');
+
+        // And return it.
+        return fetchResponse;
+      } catch (e) {
+        // Fetching didn't work get the resource from the cache.
+        const cachedResponse = await cache.match(event.request);
+        console.log('Hors ligne');
+
+        // And return it.
+        return cachedResponse;
+      }
+    })()
+  );
+});
